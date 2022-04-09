4b18a3c7c3155036c341b0fa42a9964e4801634d
shawnonthemove <1026187367@qq.com>
2022-04-09 18:15:53 +0800

    实现compose的两种方式（更改）

--- myCompose.js
+++ myCompose.js
@@ -1,16 +1,20 @@
 // 实现函数组合compose（fn1，fn2，fn3...）
+// 使用for循环实现compose存在问题，而reduce没问题
+// -----------------------------------------
 
 function myCompose(...fn) {
-  //if (fn.length === 0) return (num) => num
-  // let re = (num) => num;
-  // for (let item of fn){
-  //   re = (num) => item(re(num))
-  // }
-  // return re;
+  if (fn.length === 0) return (num) => num
+  return function () {
+    let re = arguments[0];
+    for (let f of fn){
+      re = f(re);
+    }
+    return re;
+  }
 
-  return fn.reduce((acc, item) => {
-    return (num) => item(acc(num))
-  });
+  // return fn.reduce((acc, item) => 
+  //   (num) => item(acc(num))
+  // , (num) => num);
 }
 
 function fn1(x) {
@@ -25,5 +29,6 @@ function fn3(x) {
 function fn4(x) {
   return x + 4;
 }
+
 const a = myCompose(fn1, fn2, fn3, fn4);
 console.log(a(1));
